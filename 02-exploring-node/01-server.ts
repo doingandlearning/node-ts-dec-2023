6323a117f1a0b4fe08580ef4a827e22e346aad8e
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-29 12:34:19 +0000

    less terse version of 01-server.ts

--- 02-exploring-node/01-server.ts
+++ 02-exploring-node/01-server.ts
@@ -1,13 +1,15 @@
 // const http = require("node:http"); CommonJS
 import http, { IncomingMessage, ServerResponse } from "node:http"; // ESM
 
-http
-  .createServer((req: IncomingMessage, res: ServerResponse) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<h1>Welcome to Node.js!!!! :) </h1>");
-    res.end();
-  })
-  .listen(3000);
+const server = http.createServer(handlerFunction);
+
+function handlerFunction(req: IncomingMessage, res: ServerResponse) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write("<h1>Welcome to Node.js!!!! :) </h1>");
+  res.end();
+}
+
+server.listen(3000);
 
 // tsx watch filename
 // tsx --watch filename
