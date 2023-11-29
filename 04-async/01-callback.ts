0a27ae3ddea1807800ffa077884a5ab9e7e08d38
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-29 15:39:58 +0000

    adds

--- 04-async/01-callback.ts
+++ 04-async/01-callback.ts
@@ -1,1 +1,24 @@
 import fs from "node:fs";
+
+type User = {
+  name: string;
+  age: string;
+  region: string;
+};
+
+type Regions = {
+  [key: string]: string[];
+};
+
+type NewsArticle = {
+  id: string;
+  headline: string;
+  content: string;
+};
+
+fs.readFile("./users.json", "utf8", (error: Error | null, userData: string) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+});
