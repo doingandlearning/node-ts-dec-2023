0a15e3eba39bc5dfc41cf97c37bdb18f01df538a
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-30 10:34:38 +0000

    adds extension

--- 05-node-modules/solution/src/main.ts
+++ 05-node-modules/solution/src/main.ts
@@ -1,4 +1,6 @@
-import { add, subtract } from "./mathFunctions";
+import { add, subtract, multiply, divide } from "./mathFunctions";
 
 console.log("Addition:", add(5, 3)); // Expected output: Addition: 8
 console.log("Subtraction:", subtract(5, 3)); // Expected output: Subtraction: 2
+console.log("Multiplication:", multiply(5, 3)); // Expected output: Multiplication: 15
+console.log("Division:", divide(15, 3)); // Expected output: Division: 5
