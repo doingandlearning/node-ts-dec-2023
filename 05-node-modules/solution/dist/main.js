6351253bba351063d32d43a10ad0866828efc509
Kevin Cunningham <kevin@kevincunningham.co.uk>
2023-11-30 10:31:48 +0000

    adds exercise solution

---
+++ 05-node-modules/solution/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mathFunctions_1 = require("./mathFunctions");
+console.log("Addition:", (0, mathFunctions_1.add)(5, 3)); // Expected output: Addition: 8
+console.log("Subtraction:", (0, mathFunctions_1.subtract)(5, 3)); // Expected output: Subtraction: 2
